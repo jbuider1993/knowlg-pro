1c3548e6f21231369d2e1512932167774e736327
weizhiqiang <598748873@qq.com>
2019-09-18 17:44:33 +0800

    手机端页面展示逻辑完成80%

---
+++ knowlg-web/src/main/resources/template/js/phonepage/index.js
@@ -0,0 +1,83 @@
+﻿
+//搜索内容
+var searchName = "";
+
+//知识库详情id,通过子页面赋值
+var rowId = "";
+
+layui.config({
+	base: basePath, 
+	version: skyeyeVersion
+}).extend({  //指定js别名
+    window: 'js/winui.window',
+}).define(['window', 'jquery', 'winui', 'form'], function (exports) {
+	
+	winui.renderColor();
+	
+	var $ = layui.$,
+		form = layui.form;
+	
+	
+	$("body").on("click", "#menuCheck", function(e){
+		var _this = $(this).find("i");
+		if(_this.hasClass("fa-outdent")){//展开
+			_this.removeClass("fa-outdent").addClass("fa-indent");
+			$(".know-type-ul").animate({left: '40%'});
+			$(".know-type-box").animate({left: '0px'});
+		}else{//缩进
+			_this.removeClass("fa-indent").addClass("fa-outdent");
+			$(".know-type-ul").animate({left: '100%'});
+			$(".know-type-box").animate({left: '100%'});
+		}
+	});
+	
+	$("body").on("click", ".know-type-box", function(e){
+		$("#menuCheck").find("i").removeClass("fa-indent").addClass("fa-outdent");
+		$(".know-type-box").animate({left: '100%'});
+		$(".know-type-ul").animate({left: '100%'});
+	});
+	
+	//搜索
+	$("body").on("click", "#searchBtn", function(e){
+		//隐藏首页内容
+		$(".main-show").hide();
+		$(".search-show").show();
+		searchName = $("#searchName").val();
+		$("#searchNameShow").html(searchName);
+		$("#searchKnowledge").attr("src", "search.html");
+	});
+	
+	//首页
+	$("body").on("click", "#mainShow", function(e){
+		$(".main-show").show();
+		//隐藏搜索内容
+		$(".search-show").hide();
+		//隐藏根据分类搜索类型
+		$(".type-search-show").hide();
+	});
+	
+	//分类
+	$("body").on("click", ".know-type-ul .checkTypeItem", function(e){
+		//隐藏菜单
+		$("#menuCheck").find("i").removeClass("fa-indent").addClass("fa-outdent");
+		$(".know-type-box").animate({left: '100%'});
+		$(".know-type-ul").animate({left: '100%'});
+		//隐藏首页内容
+		$(".main-show").hide();
+		//隐藏搜索按钮
+		$(".search-box").hide();
+		//隐藏搜索内容
+		$(".search-show").hide();
+		//显示类型搜索
+		$(".type-search-show").show();
+		$("#searchTypeNameShow").html($(this).html());
+		$("#searchTypeKnowledge").attr("src", "typesearch.html");
+	});
+	
+	//返回
+	$("body").on("click", "#reback", function(e){
+		$(".content-detail-show").animate({left: '100%'});
+	});
+	
+    exports('phonepageindex', {});
+});
---
+++ knowlg-web/src/main/resources/template/js/phonepage/main.js
@@ -0,0 +1,55 @@
+
+layui.config({
+	base: basePath, 
+	version: skyeyeVersion
+}).extend({  //指定js别名
+    window: 'js/winui.window',
+}).define(['window', 'table', 'jquery', 'winui', 'form'], function (exports) {
+	
+	winui.renderColor();
+	
+	var $ = layui.$,
+		form = layui.form,
+		table = layui.table;
+	
+	//表格渲染
+	table.render({
+	    id: 'messageTable',
+	    elem: '#messageTable',
+	    method: 'post',
+	    url: reqBasePath + 'knowledgecontent001',
+	    where: {title: "", state: ""},
+	    page: false,
+	    cols: [[
+	        { field: 'title', title: '', align: 'left', width: "calc(100% - 90px)", templet: function(d){
+        		return "<a style='' lay-event='details'>" + d.title + "</a>";
+	        }},
+	        { field: 'createTime', title: '', align: 'center', width: 90 },
+	    ]],
+	    done: function(){
+	    	$("#messageTable").parent().find("div[class='layui-table-header']").remove();
+	    	var tbody = $("#messageTable").parent().find("div[class='layui-table-box']");
+	    	tbody.find("td[data-field='title']").css({width: tbody.width() - 90});
+	    	tbody.find('td').css({
+	    		'border-right': 'none'
+	    	});
+	    	tbody.parent().css({
+	    		'border': 'none'
+	    	});
+	    	parent.$("#newKnowledge").height($("body").height());
+	    }
+	});
+	
+	table.on('tool(messageTable)', function (obj) { //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+        var data = obj.data; //获得当前行数据
+        var layEvent = obj.event; //获得 lay-event 对应的值
+        if (layEvent === 'details') { //详情
+        	parent.rowId = data.id;
+        	parent.$(".content-detail-show").animate({left: '0px'});
+        	parent.$("#contentShow").attr("src", "details.html");
+        }
+    });
+	
+	
+    exports('phonepagemain', {});
+});
